395eb23e64b734a5bb7c1503c2ff08ebc731e248
Hey-Viswa <viswaranjan.dev@gmail.com>
2025-06-28 08:36:57 +0000

    added folders

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,7 @@
 "use client";
 
-import Button from "@/components/Button";
-import ClickCount from "@/components/ClickCount";
-import { useState } from "react";
+
 
 export default function Home() {
-  const [count, setCount] = useState(0);
-
-  return <main>
-    
-  </main>
+  return <>Home Page</>;
 }
